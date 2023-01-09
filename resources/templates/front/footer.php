83660b8969f52a32157dfed5ef45e57b525c7cbd
Adeola Mubarak <37848504+Chessdeen@users.noreply.github.com>
2023-01-09 23:47:52 +0100

    Update footer.php

--- resources/templates/front/footer.php
+++ resources/templates/front/footer.php
@@ -6,7 +6,9 @@
         <footer>
             <div class="row">
                 <div class="col-lg-12">
-                    <p>&copy; <?php echo date("Y"); ?> Safeway Dermatology and Laser Centre | <a href="/../terms" target="_blank">Terms</a> | <a href="/../privacy" target="_blank">Privacy</a> | <a href="/../faq" target="_blank">FAQ</a> | <a href="logistics" target="_blank">Logistics</a></p>
+                    <p><a href="/" class="mb-3 me-2 mb-md-0 text-muted text-decoration-none lh-1">
+        <img src="../assets/images/logo.png" width="40" height="40" alt="safeway logo">
+      </a> &copy; <?php echo date("Y"); ?> Safeway Dermatology and Laser Centre | <a href="/../terms" target="_blank">Terms</a> | <a href="/../privacy" target="_blank">Privacy</a> | <a href="/../faq" target="_blank">FAQ</a> | <a href="logistics" target="_blank">Logistics</a></p>
                 </div>
             </div>
         </footer>
